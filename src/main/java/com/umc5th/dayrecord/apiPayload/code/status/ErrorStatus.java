0935b7044a0f28bae92b1f44cfc885b6ae5400b7
gmelan <gmelanals@gmail.com>
2024-01-07 17:33:28 +0900

    feat: 공통 API 오류 응답 코드 _NOT_FOUND 추가

--- src/main/java/com/umc5th/dayrecord/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/umc5th/dayrecord/apiPayload/code/status/ErrorStatus.java
@@ -13,8 +13,9 @@ public enum ErrorStatus implements BaseErrorCode {
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 오류"),
     _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON400","잘못된 요청입니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","인증이 필요합니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.")
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
+    _NOT_FOUND(HttpStatus.FORBIDDEN, "COMMON404", "요청을 찾을 수 없습니다.")
     ;
 
 
