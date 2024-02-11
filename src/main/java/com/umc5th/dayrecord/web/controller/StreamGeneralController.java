341dbd708e7dad61e71603cd7f72ac84b005bb71
gmelan <gmelan@gnu.ac.kr>
2024-02-11 16:32:14 +0900

    feat:

--- src/main/java/com/umc5th/dayrecord/web/controller/StreamGeneralController.java
+++ src/main/java/com/umc5th/dayrecord/web/controller/StreamGeneralController.java
@@ -2,9 +2,12 @@ package com.umc5th.dayrecord.web.controller;
 
 
 import com.umc5th.dayrecord.apiPayload.ApiResponse;
+import com.umc5th.dayrecord.service.AutoIndexService;
 import com.umc5th.dayrecord.service.StreamService.StreamCommandService;
+import com.umc5th.dayrecord.web.dto.AutoIndexDTO;
 import com.umc5th.dayrecord.web.dto.StreamDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +18,15 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class StreamGeneralController {
     private final StreamCommandService streamCommandService;
+    private final AutoIndexService autoIndexService;
 
     @PutMapping("/stream/change-stream-keyword")
     public ApiResponse<?> changeStreamKeyword(@RequestBody @Valid StreamDTO.ChangeStreamKeywordRequestDTO request) {
         return ApiResponse.onSuccess(streamCommandService.changeStreamKeyword(request));
+    }
+
+    @GetMapping("/stream/auto-indexing")
+    public ApiResponse<?> autoIndex(@RequestBody @Valid AutoIndexDTO.AutoIndexRequestDTO request) {
+        return ApiResponse.onSuccess(autoIndexService.autoIndex(request));
     }
 }
